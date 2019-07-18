3f427ead9dd5c7e13022a08b4a810fc0ff180aa8
Alasdair <alasdair123@hotmail.co.uk>
2019-07-18 17:57:10 +0100

    finished

--- lib/brackets.rb
+++ lib/brackets.rb
@@ -8,17 +8,16 @@ class Brackets
 
   def solution(parentheses, index)
     parentheses = parentheses.split('')
-
     if parentheses[index] == ')'
       p -1
     else
       parentheses.drop(index + 1).each do |x|
         @open_counter += 1 if x == '('
         @close_counter += 1 if x == ')'
-        break if @open_counter == @close_counter
+        break if @open_counter == @close_counter &&
+                 parentheses[(@open_counter * 2) + index + 1] == ')'
       end
       p (@open_counter * 2) + index + 1
-
     end
   end
 end
--- spec/bracket_spec.rb
+++ spec/bracket_spec.rb
@@ -16,8 +16,23 @@ describe Brackets do
     it 'returns 4 when given ((()())()) & 3 ' do
       expect(subject.solution('((()())())', 3)).to eq(-1)
     end
+
+    it 'returns 4 when given ((()())()) & 7' do
+      expect(subject.solution('((()())())', 7)).to eq(8)
+    end
+
+    it 'returns 6 when given ((()())) & 1' do
+      expect(subject.solution('((()()))', 1)).to eq(6)
+    end
+
+    it 'returns 6 when given ()(()())((())) & 2' do
+      expect(subject.solution('()(()())((()))', 2)).to eq(7)
+    end
+
+    it 'ANTS TEST' do
+      expect(subject.solution('()()()()((()())())', 8)).to eq(17)
+    end
   end
 end
 
-# solution('((()())())', 3) # answer = -1
 # solution('((()())())', 7) # answer = 8
